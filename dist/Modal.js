fb3baa44c019b464095142f7f58ecd0c4427d361
Gauthier Chabanne <gauthierchabanne@hotmail.fr>
2023-11-22 19:09:38 +0100

    Still trying to fix this

--- dist/Modal.js
+++ dist/Modal.js
@@ -4,10 +4,8 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
-var _react = _interopRequireDefault(require("react"));
 require("./Modal.css");
 var _jsxRuntime = require("react/jsx-runtime");
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 function Modal(_ref) {
   let {
     open,
