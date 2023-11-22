b29c0ba080c578a0d7462b05965a7a40e974e320
Gauthier Chabanne <gauthierchabanne@hotmail.fr>
2023-11-22 15:45:56 +0100

    Component done

---
+++ dist/Modal.js
@@ -0,0 +1,25 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+require("./Modal.css");
+const Modal = _ref => {
+  let {
+    open,
+    onClose
+  } = _ref;
+  if (!open) return null;
+  return /*#__PURE__*/React.createElement("div", {
+    className: "modal"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "overlay"
+  }), /*#__PURE__*/React.createElement("div", {
+    className: "modal-content"
+  }, /*#__PURE__*/React.createElement("h2", null, "Employee Created !"), /*#__PURE__*/React.createElement("button", {
+    onClick: onClose,
+    className: "close-modal"
+  }, "Close")));
+};
+var _default = exports.default = Modal;
---
+++ src/lib/Modal.js
@@ -0,0 +1,18 @@
+import "./Modal.css";
+
+const Modal = ({ open, onClose }) => {
+  if (!open) return null;
+  return (
+    <div className="modal">
+      <div className="overlay"></div>
+      <div className="modal-content">
+        <h2>Employee Created !</h2>
+        <button onClick={onClose} className="close-modal">
+          Close
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
