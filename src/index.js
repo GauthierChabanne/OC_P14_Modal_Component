41738ba29b500b49cec8ab85d2cef13aeaa72252
Gauthier Chabanne <gauthierchabanne@hotmail.fr>
2023-12-06 10:03:41 +0100

    Fixes

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
-import Modal from "./lib/Modal";
+import "./Modal.css";
+
+function Modal({ open, onClose }){
+  console.log("hello how u doing")
+  if (!open) return null;
+  return (
+    <div className="modal">
+      <div className="overlay"></div>
+      <div className="modal-content">
+        <h2>Employee Created !</h2>
+        <button onClick={onClose} className="close-modal">
+          Close
+        </button>
+      </div>
+    </div>
+  );
+};
 export { Modal };
